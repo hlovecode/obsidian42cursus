a6dcad2cefb8aaf0928b167e4ce831cb17a60ec9
LEI <lei@laptop-lei-mac.home>
2026-08-12 14:44:05 +0200

    vault backup: 2026-08-12 14:44:05

--- scripts/build_site.py
+++ scripts/build_site.py
@@ -96,7 +96,7 @@ def copy_markdown_files(
 def create_homepage():
     homepage = """# 42 Common Core Notes
 
-Welcome to my 42 Common Core learning notes.
+## Welcome to my 42 Common Core learning notes -- Hua
 
 ## Languages
 
